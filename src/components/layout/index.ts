62e0dac8ee542e36911ae0bf50a547f680ca005c
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-03-17 22:02:25 -0300

    Total revenue charts ready

---
+++ src/components/layout/index.ts
@@ -0,0 +1,7 @@
+export * from "./sider";
+export * from "./layout";
+export * from "./title";
+export * from "./header";
+export * from "./layout";
+export * from "./sider";
+export * from "./title";
