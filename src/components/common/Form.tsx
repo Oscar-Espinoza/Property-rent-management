603515b43154dae43cb9e8ad39aa230212c98714
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-03-19 23:05:43 -0300

    Create property Form added

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -76,8 +76,69 @@ const Form = ({ type, register, handleSubmit, handleImageChange, formLoading, on
                 <MenuItem value="chalet">Chalet</MenuItem>
               </Select>
             </FormControl>
+            <FormControl>
+            <FormHelperText sx={{
+                fontWeight: '500',
+                margin: '10px 0',
+                fontSize: 16,
+                color: '#11142D'
+              }}>Enter Property price</FormHelperText>
+            <TextField
+              fullWidth
+              required
+              id="outlined-basic"
+              color="info"
+              variant="outlined"
+              type="number"
+              {...register('price', { required: true })}
+            />
+          </FormControl>
           </Stack>
-          
+
+          <FormControl>
+            <FormHelperText sx={{
+                fontWeight: '500',
+                margin: '10px 0',
+                fontSize: 16,
+                color: '#11142D'
+              }}>Enter location</FormHelperText>
+            <TextField
+              fullWidth
+              required
+              id="outlined-basic"
+              color="info"
+              variant="outlined"
+              {...register('location', { required: true })}
+            />
+          </FormControl>
+
+          <Stack direction="column" gap={1} justifyContent="center" mb={2}>
+            <Stack direction="row" gap={2}>
+              <Typography color="#11142D" fontSize={16} fontWeight={500} my="10px">Property Photo</Typography>
+
+              <Button component="label" sx={{ width: 'fit-content', color: "#2ED480", textTransform: 'capitalize', fontSize: 16 }}>
+                Upload *
+                <input
+                  hidden
+                  accept="image/"
+                  type="file"
+                  onChange={(e) => {
+                    // @ts-ignore
+                    handleImageChange(e.target.files[0])
+                  }}
+                />
+              </Button>
+            </Stack>
+            
+            <Typography fontSize={14} color="#808191" sx={{ wordBreak: 'break-all' }}>{propertyImage?.name}</Typography>
+          </Stack>
+
+          <CustomButton
+            type="submit"
+            title={formLoading ? 'Submitting...' : 'Submit'}
+            backgroundColor="#475BE8"
+            color="#FCFCFC"
+          />
         </form>
       </Box>
     </Box>
--- src/pages/create-property.tsx
+++ src/pages/create-property.tsx
@@ -11,9 +11,21 @@ const CreateProperty = () => {
   const [propertyImage, setPropertyImage] = useState({ name: '', url: '' });
   const { refineCore: { onFinish, formLoading }, register, handleSubmit } = useForm();
 
-  const handleImageChange = () => {};
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) => new Promise<string>((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => resolve(fileReader.result as string);
+      fileReader.readAsDataURL(readFile);
+    });
 
-  const onFinishHandlerr = () => {};
+    reader(file).then((result: string) => setPropertyImage({ name: file?.name, url: result }));
+  };
+
+  const onFinishHandlerr = async (data: FieldValues) => {
+    if(!propertyImage.name) return alert('Please select an image');
+
+    await onFinish({ ...data, photo: propertyImage.url, email: user.email })
+  };
 
   return (
     <Form
