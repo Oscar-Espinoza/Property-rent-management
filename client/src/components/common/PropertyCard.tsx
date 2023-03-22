390f571d969ff6140ad897b483ddfe0545983013
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-03-21 23:48:05 -0300

    Property CRUD finished

--- client/src/components/common/PropertyCard.tsx
+++ client/src/components/common/PropertyCard.tsx
@@ -7,7 +7,7 @@ const PropertyCard = ({ id, title, location, price, photo }: PropertyCardProps) 
   return (
     <Card
       component={Link}
-      to={`/property/${id}`}
+      to={`/properties/show/${id}`}
       sx={{
         maxWidth: '330px',
         padding: '10px',
@@ -15,7 +15,6 @@ const PropertyCard = ({ id, title, location, price, photo }: PropertyCardProps) 
           boxShadow: '0 22px 45px 2px rgba(176, 176, 176, 0.1)',
         },
         cursor: 'pointer',
-        textDecoration: 'none',
       }}
       elevation={0}
     >
