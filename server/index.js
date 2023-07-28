39903cd85853c2c2526981879cd4cbffe9a0aa70
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-07-28 19:59:27 -0300

    ConnectDB function updated to wait the connection to the db

--- server/index.js
+++ server/index.js
@@ -2,11 +2,21 @@ import express from 'express';
 import * as dotenv from  'dotenv';
 import cors from 'cors';
 
-import connectDB from './mongodb/connect.js';
 import userRouter from './routes/user.routes.js';
 import propertyRouter from './routes/property.routes.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
 const app =  express();
 app.use(cors());
@@ -19,14 +29,21 @@ app.get('/', (req, res) => {
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/properties', propertyRouter);
 
-const startSever = async () => {
-  try {
-    connectDB(process.env.MONGODB_URL);
+// const startSever = async () => {
+//   try {
+//     await connectDB(process.env.MONGODB_URL);
+//     app.listen(8080, () => console.log('Server has started on port http://localhost:8080'));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-    app.listen(8080, () => console.log('Server has started on port http://localhost:8080'));
-  } catch (error) {
+connectDB()
+  .then(() => {
+    app.listen(8080, () => {
+        console.log("listening for requests");
+    });
+  })
+  .catch(error => {
     console.log(error);
-  }
-};
-
-startSever();
+  });
